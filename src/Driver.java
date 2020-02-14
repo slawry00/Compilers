2830b4d64769850517def51c235c4abfc2195b3d
ChrisCooper441 <day0fdiligence@gmail.com>
2020-02-14 01:28:17 -0700

    working driver file

--- src/Driver.java
+++ src/Driver.java
@@ -1,52 +1,42 @@
+// CSCI 468
+// Chris Cooper, Spencer Lawry, Matthew Gober
 
-
-import org.antlr.v4.runtime.*;
-import java.util.*;
 import java.io.*;
-
-import static jdk.nashorn.internal.objects.NativeSet.size;
-
+import java.util.*;
+import org.antlr.v4.runtime.*;
 
 public class Driver {
-    public static void main(String[] args) throws Exception {
 
-//        String in_file = args[0]; // get filename from cmd line
-        System.out.println("asdf");
+    public static void main(String[] args) throws Exception {
+        String fileName = "inputs/loop.micro";      // set later to args[0] and setup in bash script
+        String outputName = "outputs/loop.out";     // set output args[0] + ".out";
+        FileWriter myWriter = new FileWriter(outputName);
 
         try {
-            // CharStream prints
-            CharStream codePointCharStream = CharStreams.fromFileName("./inputs/fibonacci.micro");
-            LittleLexer lexer = new LittleLexer(codePointCharStream);
+            // create the grammar from CharStream interface
+            LittleLexer grammar = new LittleLexer(CharStreams.fromFileName(fileName));
+            // create the token stream and set channel to 0 to pull tokens instead of default channel
+            // (default doesn't pull tokens correctly)
+            CommonTokenStream tokens = new CommonTokenStream(grammar, 0);
+            tokens.getNumberOfOnChannelTokens();        //iterates through and counts EOF once
+            // create list of tokens and utilize the Vocabulary interface to return string associated with token
+            List<Token> tokenList = tokens.getTokens();
+            Vocabulary vocab = grammar.getVocabulary();
+            for(int i=0; i < tokenList.size(); i++){
+                int type = tokenList.get(i).getType();
 
-
-            LittleParser parser = new LittleParser(new CommonTokenStream(lexer));
-            parser.addParseListener(new LittleBaseListener());
-
-            // Start parsing
-            Vocabulary vocab = parser.getVocabulary();
-            System.out.println(vocab);
-
-//            ANTLRFileStream in_stream = fromFileName(in_file);// turn into an antlr CharStream
-//            LittleLexer lexer = new LittleLexer(in_stream);
-//
-//            CommonTokenStream tok_s = new CommonTokenStream(lexer);
-//            List<String> tok_list = tok_s.getAllTokens()
-
-            for(int i=0; i < size(vocab); i++){
-                System.out.println(vocab.getDisplayName(i) + ":" + vocab.getSymbolicName(i));
-                FileWriter(vocab.getDisplayName(i) + ":" + vocab.getSymbolicName(i));
+                // EOF is 0, so exclude it from output
+                if (type > 0) {
+                    String symbol = vocab.getSymbolicName(type);
+                    System.out.println("Token Type: " + symbol);
+                    System.out.println("Value: " + tokenList.get(i).getText());
+                    myWriter.append("Token Type: " + symbol);
+                    myWriter.append("Value: " + tokenList.get(i).getText());
+                }
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    // output to output.out
-    public static void FileWriter(String content) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("output.out", true));
-        writer.write(content);
-        writer.close();
-
     }
 }
